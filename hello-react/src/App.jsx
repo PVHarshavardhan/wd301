18466a541d8001a87d6b2c40e5a9b0f73db8850a
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-10 09:47:58 +0530

    again level1

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -8,14 +8,14 @@ function App() {
     <div className = "mainroot">
       
       <div>
-        <h1 className="h1">Pending</h1>
+      <h1 style={{marginBottom:'10px',fontSize:'35px',fontWeight:'bold'}}>Pending</h1>
         <div>
         <TaskCard title = "build the website with react.." dueDate="10th april"assigneeName="Rohit S" type="pending"  />
         <TaskCard title = "add blog" dueDate="22nd March"assigneeName="Rohit M" type="pending" />
         </div>
       </div>
       <div>
-      <h1 className="h1">Done</h1>
+      <h1 style={{marginBottom:'10px',fontSize:'35px',fontWeight:'bold'}}>Done</h1>
       <div>
         <TaskCard title ="design the mock up" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
         <TaskCard title ="get approval" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
