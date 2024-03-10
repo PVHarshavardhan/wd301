601e63ac70c7f43ab5acb57ff51c75110eade4f0
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-10 09:36:07 +0530

    again level1

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -4,22 +4,24 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    
     <div className = "mainroot">
       
       <div>
         <h1 className="h1">Pending</h1>
+        <div>
         <TaskCard title = "build the website with react.." dueDate="10th april"assigneeName="Rohit S" type="pending"  />
         <TaskCard title = "add blog" dueDate="22nd March"assigneeName="Rohit M" type="pending" />
+        </div>
       </div>
       <div>
       <h1 className="h1">Done</h1>
+      <div>
         <TaskCard title ="design the mock up" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
         <TaskCard title ="get approval" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
-        
+      </div>
       </div>
     </div>
-    </>
   )
 }
 
