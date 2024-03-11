b40bbe452733a6df0281d118644aeb6a7f9d372f
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-11 07:13:41 +0530

    again m1

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,22 +1,21 @@
 import TaskCard from "./TaskCard"
-import './App.css'
+
 
 function App() {
 
   return (
     
-    <div className = "mainroot">
-      
-      <div>
+    <div style={{display:"flex", margin:"5px",width:'100%'}}>
+      <div style={{margin:"15px",width:'50%'}}>
       <h1 style={{marginBottom:'10px',fontSize:'35px',fontWeight:'bold'}}>Pending</h1>
         <div>
         <TaskCard title = "build the website with react.." dueDate="10th april"assigneeName="Rohit S" type="pending"  />
         <TaskCard title = "add blog" dueDate="22nd March"assigneeName="Rohit M" type="pending" />
         </div>
       </div>
-      <div>
+      <div style={{margin:"15px",width:'50%'}}>
       <h1 style={{marginBottom:'10px',fontSize:'35px',fontWeight:'bold'}}>Done</h1>
-      <div>
+      <div >
         <TaskCard title ="design the mock up" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
         <TaskCard title ="get approval" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
       </div>
