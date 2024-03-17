7e28fa5ec2cf803d91627665f17118354709eb9e
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-17 21:50:41 +0530

    level 2

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -10,15 +10,15 @@ function App() {
       <div style={{margin:"15px",width:'50%'}}>
       <h1 style={{marginBottom:'10px',fontSize:'35px',fontWeight:'bold'}}>Pending</h1>
         <div>
-        <TaskCard title = "build the website with react.." dueDate="10th april"assigneeName="Rohit S" type="pending"  />
-        <TaskCard title = "add blog" dueDate="22nd March"assigneeName="Rohit M" type="pending" />
+        <TaskCard title = "build the website with react.." dueDate="10th april" assigneeName="Rohit S"  />
+        <TaskCard title = "add blog" dueDate="22nd March" assigneeName="Rohit M" />
         </div>
       </div>
       <div style={{margin:"15px",width:'50%'}}>
       <h1 style={{marginBottom:'10px',fontSize:'35px',fontWeight:'bold'}}>Done</h1>
       <div >
-        <TaskCard title ="design the mock up" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
-        <TaskCard title ="get approval" completedAtDate="10th april"assigneeName="Rohit m" type="done" />
+        <TaskCard title ="design the mock up" completedAtDate ="10th april" assigneeName="Rohit m"  />
+        <TaskCard title ="get approval" completedAtDate ="10th april" assigneeName="Rohit m"  />
       </div>
       </div>
     </div>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 
 interface TaskCardprops {
     title: string;
-    completdAtDate?: string;
+    completedAtDate?: string;
     dueDate?:string;
-    assignName:string;
+    assigneeName:string;
 }
-const TaskCard = (props) => {
-    const {title, dueDate, completedAtDate, assigneeName } = props;
+const TaskCard :React.FC<TaskCardprops>= (props) => {
+    const {title,completedAtDate, dueDate, assigneeName } = props;
 
     return  (
         <div className='TaskItem'>
-            <h2 className="text-xl font-bold">{title}</h2>
+            <h2 className="title">{title}</h2>
             <p className='Date'>{(dueDate) ?  `Due on: ${dueDate}` : `Completed on: ${completedAtDate}` }</p>
             <p className='Assigne'>Assignee :{assigneeName}</p>
         </div>
