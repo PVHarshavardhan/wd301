900fb458de9725a905b144dcf413cc29599e2db2
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-17 21:42:13 +0530

    level 2

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,5 +1,13 @@
 import './TaskCard.css'
 import React from 'react';
+
+
+interface TaskCardprops {
+    title: string;
+    completdAtDate?: string;
+    dueDate?:string;
+    assignName:string;
+}
 const TaskCard = (props) => {
     const {title, dueDate, completedAtDate, assigneeName } = props;
 
@@ -7,7 +15,7 @@ const TaskCard = (props) => {
         <div className='TaskItem'>
             <h2 className="text-xl font-bold">{title}</h2>
             <p className='Date'>{(dueDate) ?  `Due on: ${dueDate}` : `Completed on: ${completedAtDate}` }</p>
-            <p>Assignee :{assigneeName}</p>
+            <p className='Assigne'>Assignee :{assigneeName}</p>
         </div>
     )
 }
