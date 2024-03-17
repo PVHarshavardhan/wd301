772d400b9398c215e2a0384b5c1e961fe5f54893
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-17 22:06:06 +0530

    level 2

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,23 +1,24 @@
 import './TaskCard.css'
 import React from 'react';
 
-
 interface TaskCardprops {
-    title: string;
-    completedAtDate?: string;
-    dueDate?:string;
-    assigneeName:string;
+  title: string;
+  completedAtDate?: string;
+  dueDate?:string;
+  assigneeName:string;
 }
-const TaskCard :React.FC<TaskCardprops>= (props) => {
-    const {title,completedAtDate, dueDate, assigneeName } = props;
 
-    return  (
-        <div className='TaskItem'>
-            <h2 className="title">{title}</h2>
-            <p className='Date'>{(dueDate) ? `Due on: ${dueDate}` : `Completed on: ${completedAtDate}` }</p>
-            <p className='Assignee'>Assignee :{assigneeName}</p>
-        </div>
-    )
+const TaskCard :React.FC<TaskCardprops> = (props) => {
+  const {title,completedAtDate,dueDate,assigneeName} = props;
+
+
+  return (
+    <div className="TaskItem">
+      <h2 className='Title'>{title}</h2>
+      <p className='Date'>{(dueDate) ? `Due on: ${dueDate}` : `Completed on: ${completedAtDate}` }</p>
+      <p className='Assignee'>Assignee: {assigneeName}</p>
+    </div>
+  )
 }
 
 export default TaskCard
