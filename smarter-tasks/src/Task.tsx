07dea012310946e1529f78c0af16010a0f62d322
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-03-31 17:50:05 +0530

    l4

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -22,7 +22,7 @@ const Task = (props: TaskProps) => {
         </div>
       
       <div style={{marginLeft:"120px",marginTop:"15px"}}>
-      <button onClick={deleteTask} id="deleteTaskButton" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Delete Task </button>
+      <button onClick={deleteTask}  className="deleteTaskButton text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Delete Task </button>
       </div>
       </div>
     </div>
