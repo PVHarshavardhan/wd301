4f6bd0a88f5a7162c5a65507a57cbec8ee93ed75
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0>
2024-04-24 21:26:34 +0530

    l7

--- smarter-tasks/src/pages/projects/ProjectListItems.tsx
+++ smarter-tasks/src/pages/projects/ProjectListItems.tsx
@@ -1,38 +1,28 @@
+import React, { useEffect } from 'react';
+import { fetchProjects } from "../../context/projects/actions";
 
+// So, let's import the useProjectsDispatch custom hook.
+import { useProjectsDispatch } from "../../context/projects/context";
 
-// First, I'll import the useProjectsState custom hook to access projects state.
-import { useProjectsState } from "../../context/projects/context";
-export default function ProjectListItems() {
+// I'll import the ProjectListItems component from the same folder. 
+// This I'll define next.
+import ProjectListItems from './ProjectListItems';
+const ProjectList: React.FC = () => {
 
-  // I'll define a new constant called `state`, to call the useProjectsState() hook, 
-  // and get access to projects state.
-  const state: any = useProjectsState();
-
-  // Next, I'll destructure the state object to gain access to projects, 
-  // isLoading, isError and errorMessage property.
-  const { projects, isLoading, isError, errorMessage } = state
-  console.log(projects);
-
-  // If `isLoading` is true, and there are no projects, in that case, 
-  // I'll show a loading text
-  if (projects.length === 0 && isLoading) {
-    return <span>Loading...</span>;
-  }
+  // I'll define a new constant called dispatchProjects, 
+  // to call the useProjectsDispatch() hook.
+  const dispatchProjects = useProjectsDispatch();
   
-// Next, if there is an error, I'll show the error message.
-  if (isError) {
-    return <span>{errorMessage}</span>;
-  }
-
-  // And finally I'll iterate over the projects object to show the 
-  // individual projects card.
+  useEffect(() => {
+    // And I'll pass the `dispatchProjects` to `fetchProjects` function.
+    fetchProjects(dispatchProjects)
+  }, [])
   return (
-    <>
-      {projects.map((project: any) => (
-        <div key={project.id} className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
-        </div>
-      ))}        
-    </>
+    <div className="grid gap-4 grid-cols-4 mt-5">
+      {/*To keep this file clean, I'll move all the logic to access the projects 
+       from our app-state, to a new component ProjectListItems */}
+      <ProjectListItems />
+    </div>
   );
-}
+};
+export default ProjectList;
